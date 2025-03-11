479c248abc7e8d4a3a63afd7ad513d0f71e56746
patrickkfkan <patrickkfkan@gmail.com>
2025-03-11 20:04:53 +0800

    Disable file logging by default

--- src/core/util/Config.ts
+++ src/core/util/Config.ts
@@ -1,12 +1,15 @@
 import { FileLogger } from "patreon-dl";
 import path from "path";
 
-/**
- * Note: we do not need this with next version of patreon-dl,
- * as FileLogger.getDefaultConfig() will return logDir with path.sep.
- */
 export function getDefaultFileLoggerOptions() {
   const config = { ...FileLogger.getDefaultConfig() };
+  config.enabled = false;
+
+  /**
+   * Note: we do not need this with next version of patreon-dl,
+   * as FileLogger.getDefaultConfig() will return logDir with path.sep.
+   */
   config.logDir = config.logDir.replaceAll("/", path.sep);
+
   return config;
 }
