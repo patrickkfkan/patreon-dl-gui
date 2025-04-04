816e93606a2d4053c6816c9f9e5b5b6738c2614c
patrickkfkan <patrickkfkan@gmail.com>
2025-04-04 20:47:24 +0800

    Update `patreon-dl` lib

--- src/core/util/Config.ts
+++ src/core/util/Config.ts
@@ -14,12 +14,6 @@ export function getDefaultFileLoggerOptions() {
   const config = { ...FileLogger.getDefaultConfig() };
   config.enabled = false;
 
-  /**
-   * Note: we do not need this with next version of patreon-dl,
-   * as FileLogger.getDefaultConfig() will return logDir with path.sep.
-   */
-  config.logDir = config.logDir.replaceAll("/", path.sep);
-
   return config;
 }
 
