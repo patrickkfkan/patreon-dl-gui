8eba5fbf9c8d0347cd4c175b370615af5fbba64d
patrickkfkan <patrickkfkan@gmail.com>
2025-05-25 00:34:53 +0800

    Fix "browser not secure" msg when signing in with Google

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import MainProcess from "./core/MainProcess";
-import { app } from "electron";
+import { app, session } from "electron";
 import electronStartup from "electron-squirrel-startup";
+import { USER_AGENT } from "./core/Constants";
 
 // Handle creating/removing shortcuts on Windows when installing/uninstalling.
 if (electronStartup) {
@@ -8,6 +9,7 @@ if (electronStartup) {
 }
 
 app.on("ready", async () => {
+  session.defaultSession.setUserAgent(USER_AGENT);
   const main = new MainProcess();
   await main.start();
 });
