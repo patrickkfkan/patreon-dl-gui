0588af93cee6f568f697887fc44df823b261ef3c
patrickkfkan <patrickkfkan@gmail.com>
2025-09-22 15:35:19 +0800

    Update `patreon-dl` lib

--- vite.main.config.ts
+++ vite.main.config.ts
@@ -38,7 +38,7 @@ export default defineConfig((env) => {
           "bufferutil",
           "utf-8-validate",
           "undici", // ^6.21.3
-          "patreon-dl", // ^3.0.0
+          "patreon-dl", // ^3.2.1
 
           ...builtinModules,
           ...builtinModules.map(m => `node:${m}`)
